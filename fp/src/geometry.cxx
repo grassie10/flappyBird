2dc2b2c4656d0251c6c0e9b7f91b168e544b6bb9
ckang2023 <cindykang2023@u.northwestern.edu>
2020-05-28 15:06:31 -0500

    completed geometry

--- fp/src/geometry.cxx
+++ fp/src/geometry.cxx
@@ -5,7 +5,7 @@
 Geometry::Geometry() noexcept
         : bird_radius    {    15 }
         , bird_boost      {    5 }
-        , bird_velocity0 {    3, -10 }
+        , bird_velocity0 {    0, -10 }
         , background_velocity  {   10,   0 }
         , scene_dims     { 1000, 700 }
         , side_margin    {  50 }
