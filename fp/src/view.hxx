718c90a182db3a98b12aaa2476d2e5a0a5714700
Grace Liu <gxliu1@graces-mbp-2.lan>
2020-05-31 21:41:58 -0500

    created view.hxx

---
+++ fp/src/view.hxx
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <ge211.hxx>
+#include "model.hxx"
+
+class View
+{
+    ///
+    /// PRIVATE MEMBER VARIABLES
+    ///
+
+public:
+
+    ///
+    /// CONSTRUCTOR
+    ///
+
+    explicit View(Model const&);
+
+    ///
+    /// MEMBER FUNCTIONS
+    ///
+};
